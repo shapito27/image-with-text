049effa37957684a6e9172e60d52f742343c070f
ruslan <legionerust@yandex.ru>
2020-03-25 00:22:29 +0800

    fix bugs; image was created, it's works

--- create-image.php
+++ create-image.php
@@ -1,8 +1,10 @@
 <?php
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Shapito27\ImageCreator\Services\ImageGenerator;
 use Shapito27\ImageCreator\Models\Color;
+
 /**
  * Plugin Name: Create Post Feature Image
  * Plugin URI: https://thisis-blog.ru/
@@ -11,10 +13,64 @@ use Shapito27\ImageCreator\Models\Color;
  * Author: Saifullin Ruslan
  * Author URI: https://thisis-blog.ru/
  **/
-add_action('post_submitbox_misc_actions', 'custom_button');
 
-function custom_button($post): string
+add_action('wp_ajax_generate_image', 'generate_image');
+
+function generate_image()
 {
+    global $wpdb; // this is how you get access to the database
+
+    $res = 'Good';
+    try {
+        $postId = intval($_POST['post_id']);
+        $post   = get_post($postId);
+
+        $resultImagePath     = 'public/images/result/digi.jpg';
+        $resultImageFullPath = __DIR__ . DIRECTORY_SEPARATOR . $resultImagePath;
+        $imageGenerator      = new ImageGenerator();
+        $result              = $imageGenerator
+            ->setSourceImagePath(__DIR__ . DIRECTORY_SEPARATOR . 'public/images/source/tv.jpg')
+            ->setResultImagePath($resultImageFullPath)
+            ->setFontPath(__DIR__ . DIRECTORY_SEPARATOR . 'public/font/merriweatherregular.ttf')
+            ->setTextColor(new Color(255, 255, 255))
+            ->setTextFontSize(25)
+            ->setText($post->post_title)//"Взыскание долга";//"Взыскание долга, неустойки, дебиторской задолженности";
+            ->setCoeficientLeftRightTextPadding(20)
+            ->setTextLinesTopBottomPadding(15)
+            ->setImageQuality(100)
+            ->generate();
+
+//    echo '<img src="' . $resultImagePath . '">';
+
+    } catch (Exception $exception) {
+        $res = $exception->getMessage();
+    }
+    //response
+    echo $res;
+
+    wp_die(); // this is required to terminate immediately and return a proper response
+}
+
+## Добавляем блоки в основную колонку на страницах постов и пост. страниц
+add_action('add_meta_boxes', 'myplugin_add_custom_box');
+function myplugin_add_custom_box()
+{
+    $screens = [
+        'post',
+//        'page'
+    ];
+    add_meta_box('myplugin_sectionid', 'Generate image', 'myplugin_meta_box_callback', $screens, 'side');
+}
+
+// HTML код блока
+function myplugin_meta_box_callback($post)
+{
+    // Используем nonce для верификации
+    wp_nonce_field(plugin_basename(__FILE__), 'create_image');
+
+//    // значение поля
+//    $value = get_post_meta( $post->ID, 'my_meta_key', 1 );
+
     if ( !has_post_thumbnail($post->ID)) {
         ?>
         <script type="text/javascript">
@@ -42,36 +98,8 @@ function custom_button($post): string
 
         echo $html;
     }
-}
 
-
-add_action('wp_ajax_generate_image', 'generate_image');
-
-function my_action()
-{
-    global $wpdb; // this is how you get access to the database
-
-    $postId = intval($_POST['post_id']);
-
-
-    $resultImagePath     = 'images/result/law2.jpg';
-    $resultImageFullPath = __DIR__ . DIRECTORY_SEPARATOR . $resultImagePath;
-    $imageGenerator      = new ImageGenerator();
-    $result              = $imageGenerator
-        ->setSourceImagePath(__DIR__ . '/images/source/law.jpg')
-        ->setResultImagePath($resultImageFullPath)
-        ->setFontPath('font/merriweatherregular.ttf')
-        ->setTextColor(new Color(255, 255, 255))
-        ->setTextFontSize(25)
-        ->setText('Взыскание долга 2')//"Взыскание долга";//"Взыскание долга, неустойки, дебиторской задолженности";
-        ->setCoeficientLeftRightTextPadding(20)
-        ->setTextLinesTopBottomPadding(15)
-        ->setImageQuality(100)
-        ->generate();
-    echo '<img src="' . $resultImagePath . '">';
-
-    //response
-//    echo $whatever;
-
-    wp_die(); // this is required to terminate immediately and return a proper response
+//    // Поля формы для введения данных
+//    echo '<label for="myplugin_new_field">' . __("Description for this field", 'myplugin_textdomain' ) . '</label> ';
+//    echo '<input type="text" id="myplugin_new_field" name="myplugin_new_field" value="'. $value .'" size="25" />';
 }
